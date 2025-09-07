df9f552fe3f092a72f46283fabbaf64bab6b4cca
Alessandro De Waele <alessandro.dewaele@student.hogent.be>
2025-09-07 14:16:54 +0200

    start vue project with vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button label="Click Me" icon="pi pi-check" />
+</template>
+
+<script>
+import Button from "primevue/button";
+
+export default {
+  components: { Button },
+};
+</script>
+
+<style scoped></style>
